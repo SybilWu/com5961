7e9be822adbb48fa8905365e89f42096866365b4
SybilWu <31612769+SybilWu@users.noreply.github.com>
2017-09-17 12:32:35 +0800

    Delete math_function.js

--- math_function.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-	var x1=prompt("Enter x1=","");
-	var x2=prompt("Enter x2 value=","");
-	var x3=prompt("Enter x3 value=","");
-	var y=(parseInt(x1)+parseInt(x2))/x3;
-	document.getElementById('local').innerHTML="x1=200,x2=300,x3=500,y=(x1+x2)/x3="+y;
-</script>
